23ff80c4d19ed2046613ccf202179a53c1290a38
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2015-10-06 23:27:09 +0200

    Minor template changes

--- client/templates/MenuLeft.js
+++ client/templates/MenuLeft.js
@@ -1,19 +1,26 @@
 Template.MenuLeft.onRendered(function() {
-	$('#__blaze-root .ui.sidebar').sidebar({
-    	context: $('#__blaze-root')
-  	}).sidebar('attach events', '#__blaze-root .menu .item')
+  $('#__blaze-root .ui.sidebar').sidebar({
+    context: $('#__blaze-root')
+  }).sidebar('attach events', '#__blaze-root .menu .item')
 })
 
 Template.MenuLeft.helpers({
 
-	items: function() {
-		return [
-			{ icon:"grid layout", hidden:"", label:"Overview", href:"/" },
-			{ icon:"sign in", 	  hidden:"", label:"Output",   href:"/" },
-			{ icon:"sitemap",     hidden:"", label:"Data"    	 		},
-			{ icon:"sign in",     hidden:"", label:"Output",   href:"/" }
-		]	
-	}
+  items: function() {
+    return [
+      { icon:"grid layout", label:"Overview", href:"/" },
+      { icon:"doctor",      label:"Data",      
+        items: [
+          { hidden:"", label:"Ph",  href:"/data/ph" },
+          { hidden:"", label:"Na",  href:"/data/na" },
+          { hidden:"", label:"Cl",  href:"/data/cl" },
+          { hidden:"", label:"K",   href:"/data/k" }
+        ]
+      },
+      { icon:"sitemap",  label:"Outputs",  href:"/outputs" },
+      { icon:"settings", label:"Options",  href:"/options" }
+    ] 
+  }
 
 })
 
