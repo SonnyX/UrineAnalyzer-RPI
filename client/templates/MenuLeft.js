13154df4befb9b3029a2fbb0a5788ccec4fa4d48
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2015-10-07 12:26:12 +0200

    Improved server code

--- client/templates/MenuLeft.js
+++ client/templates/MenuLeft.js
@@ -1,8 +1,10 @@
+/*
 Template.MenuLeft.onRendered(function() {
   $('#__blaze-root .ui.sidebar').sidebar({
     context: $('#__blaze-root')
   }).sidebar('attach events', '#__blaze-root .menu .item')
 })
+*/
 
 Template.MenuLeft.helpers({
 
