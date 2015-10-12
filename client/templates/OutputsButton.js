dd53a76cd5415650b7980037dfc191dfa0688733
BernardoS <bernardo.lucas@globo.com>
2015-10-12 10:41:24 +0200

    Electron added and minor changes to make it run in first instance were added

--- client/templates/OutputsButton.js
+++ client/templates/OutputsButton.js
@@ -1,24 +1,34 @@
 var LOW = 0 , HIGH = 255;
 
-OutputsButtonsCol = new Mongo.Collection(null);
 var groups = [
-  {name:'Radiobox',items:[
+  {template:'Radiobox',items:[
     {label:"pH",pins:[11,12,13],values:[LOW,HIGH,HIGH]},
     {label:"Cl-",pins:[11,12,13],values:[LOW,HIGH,LOW]},
     {label:"K+",pins:[11,12,13],values:[HIGH,LOW,LOW]},
     {label:"Na+",pins:[11,12,13],values:[LOW,LOW,LOW]},
   ]},
-  {name:'Checkbox',items:[
+  {template:'Checkbox',items:[
     {label:'Motor',pins:[8],values:[HIGH]}
   ]}
 ]
-groups.forEach(function(group){
-  OutputsButtonsCol.insert(group);
+
+var items = [
+  {label:"pH",pins:[11,12,13],values:[LOW,HIGH,HIGH],group:'sensor'},
+  {label:"Cl-",pins:[11,12,13],values:[LOW,HIGH,LOW],group:'sensor'},
+  {label:"K+",pins:[11,12,13],values:[HIGH,LOW,LOW],group:'sensor'},
+  {label:"Na+",pins:[11,12,13],values:[LOW,LOW,LOW],group:'sensor'},
+  {label:'Motor',pins:[8],values:[HIGH],group:'motor'},
+  {label:'Motor',pins:[8],values:[LOW],group:'motor'}
+]
+
+OutputsButtons = new Mongo.Collection(null);
+items.forEach(function(item){
+  OutputsButtons.insert(item);
 })
 
 Template.Form.helpers({
   Groups: function(){
-    return OutputsButtonsCol.find({});
+    return groups;
   }
 });
 
@@ -26,11 +36,11 @@ Template.Form.events({
   "click .ui.toggle.checkbox": function(event, template){
     if ($(event.currentTarget).checkbox("is checked"))
     {
-      Meteor.call('changeOutput',this.pins,this.values)
+      //Meteor.call('changeOutput',this.pins,this.values)
     }
     else {
       var values = Array.apply(null, Array(this.values.length)).map(function(){return LOW});
-      Meteor.call('changeOutput',this.pins,values)
+      //Meteor.call('changeOutput',this.pins,values)
     }
   }
 });
@@ -39,29 +49,3 @@ Template.Form.onRendered(function () {
   $('.Radiobox.ui').checkbox('attach events', 'label.Radiobox', 'check');
   $('.Checkbox.ui').checkbox('can change');
 });
-
-
-/*
-Serial.println("Now measuring pH");
-digitalWrite(11, LOW);
-digitalWrite(12, HIGH);
-digitalWrite(13, HIGH);
-} else if(inputString=="c") {
-Serial.println("Now measuring Cl-");
-digitalWrite(11, LOW);
-digitalWrite(12, HIGH);
-digitalWrite(13, LOW);
-} else if(inputString=="k") {
-Serial.println("Now measuring K+");
-digitalWrite(11, HIGH);
-digitalWrite(12, LOW);
-digitalWrite(13, LOW);
-} else if(inputString=="n") {
-Serial.println("Now measuring Na+");
-digitalWrite(11, LOW);
-digitalWrite(12, LOW);
-digitalWrite(13, LOW);
-} else if(inputString=="m") {
-Serial.println("Switched Motor On/Off");
-digitalWrite(8,!digitalRead(8));
-*/
